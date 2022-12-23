98c6d8f8360514f61c76360bf8f7e5503faf1290
Paweł Dębski <pawel.debski.99@gmail.com>
2022-12-23 18:57:22 +0100

    Initialized project

---
+++ BuD/BuD/bud_pch.cpp
@@ -0,0 +1,1 @@
+#include "bud_pch.h"
---
+++ BuD/BuD/bud_pch.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+
+// std
+#include <memory>
+#include <algorithm>
+#include <execution>
+#include <functional>
+
+// data structures
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
