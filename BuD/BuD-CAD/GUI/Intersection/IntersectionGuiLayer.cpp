83a81939144c2a4d2187b34d2c06cc946e6e6849
Paweł Dębski <pawel.debski.99@gmail.com>
2023-08-29 11:41:31 +0200

    Added second intersection curve on intersection detection

--- BuD/BuD-CAD/GUI/Intersection/IntersectionGuiLayer.cpp
+++ BuD/BuD-CAD/GUI/Intersection/IntersectionGuiLayer.cpp
@@ -70,47 +70,71 @@ void IntersectionGuiLayer::DrawGui()
 
 			if (intersectionResult.m_IntersectionFound)
 			{
-				auto& startingPoint = intersectionResult.m_StartingPoint;
-
-				std::vector<IntersectionPoint> intersectionPoints1 = {};
-				std::vector<IntersectionPoint> intersectionPoints2 = {};
-
-				float prevWrappedU = 0.0f, prevWrappedV = 0.0f;
-
-				std::transform(
-					intersectionResult.m_BackwardsPoints.rbegin(), intersectionResult.m_BackwardsPoints.rend(),
-					std::back_inserter(intersectionPoints1),
-					[&prevWrappedU, &prevWrappedV](const NextCommonPointResult& point)
-					{
-						auto result = IntersectionPoint({ point.m_Parameter.x, point.m_Parameter.y }, prevWrappedU, prevWrappedU);
-						prevWrappedU = -point.m_WrappedU;
-						prevWrappedV = -point.m_WrappedV;
-
-						return result;
-					}
-				);
-
-				intersectionPoints1.emplace_back(dxm::Vector2{ startingPoint.m_Parameter.x, startingPoint.m_Parameter.y }, prevWrappedU, prevWrappedV);
-
-				std::transform(
-					intersectionResult.m_ForwardPoints.begin(), intersectionResult.m_ForwardPoints.end(),
-					std::back_inserter(intersectionPoints1),
-					[](const NextCommonPointResult& point)
-					{
-						return IntersectionPoint({ point.m_Parameter.x, point.m_Parameter.y }, point.m_WrappedU, point.m_WrappedV);
-					}
-				);
-
-				if (intersectionResult.m_LoopDetected.has_value())
+				auto converter = [](
+					const IntersectionResult& result,
+					std::function<dxm::Vector2(const dxm::Vector4&)> uvGetter,
+					std::function<dxm::Vector2(const NextCommonPointResult&)> uvWrappedGetter,
+					std::function<dxm::Vector2(const LoopResult&)> loopWrappedGetter
+				)
 				{
-					intersectionPoints1.emplace_back(
-						dxm::Vector2{ startingPoint.m_Parameter.x, startingPoint.m_Parameter.y }, 
-						intersectionResult.m_LoopDetected->m_WrappedU,
-						intersectionResult.m_LoopDetected->m_WrappedV
+					std::vector<IntersectionPoint> intersectionPoints = {};
+					dxm::Vector2 prevWrapped = { 0.0f, 0.0f };
+
+					std::transform(
+						result.m_BackwardsPoints.rbegin(), result.m_BackwardsPoints.rend(),
+						std::back_inserter(intersectionPoints),
+						[&prevWrapped, &uvGetter, &uvWrappedGetter](const NextCommonPointResult& point)
+						{
+							auto result = IntersectionPoint(uvGetter(point.m_Parameter), prevWrapped.x, prevWrapped.y);
+							
+							prevWrapped = uvWrappedGetter(point);
+
+							return result;
+						}
 					);
-				}
+
+					intersectionPoints.emplace_back(uvGetter(result.m_StartingPoint.m_Parameter), prevWrapped.x, prevWrapped.y);
+
+					std::transform(
+						result.m_ForwardPoints.begin(), result.m_ForwardPoints.end(),
+						std::back_inserter(intersectionPoints),
+						[&uvGetter, &uvWrappedGetter](const NextCommonPointResult& point)
+						{
+							auto wrapped = uvWrappedGetter(point);
+							return IntersectionPoint(uvGetter(point.m_Parameter), wrapped.x, wrapped.y);
+						}
+					);
+
+					if (result.m_LoopDetected.has_value())
+					{
+						auto loopWrapped = loopWrappedGetter(result.m_LoopDetected.value());
+
+						intersectionPoints.emplace_back(
+							uvGetter(result.m_StartingPoint.m_Parameter),
+							loopWrapped.x,
+							loopWrapped.y
+						);
+					}
+
+					return intersectionPoints;
+				};
+
+				auto intersectionPoints1 = converter(
+					intersectionResult, 
+					[](const dxm::Vector4& parameter) { return dxm::Vector2{ parameter.x, parameter.y }; },
+					[](const NextCommonPointResult& result) { return dxm::Vector2{ result.m_WrappedU, result.m_WrappedV }; },
+					[](const LoopResult& result) { return dxm::Vector2{ result.m_WrappedU, result.m_WrappedV }; }
+				);
+
+				auto intersectionPoints2 = converter(
+					intersectionResult,
+					[](const dxm::Vector4& parameter) { return dxm::Vector2{ parameter.z, parameter.w }; },
+					[](const NextCommonPointResult& result) { return dxm::Vector2{ result.m_WrappedS, result.m_WrappedT }; },
+					[](const LoopResult& result) { return dxm::Vector2{ result.m_WrappedS, result.m_WrappedT }; }
+				);
 
 				m_MainDataLayer.m_SceneDataLayer.m_SceneCAD.CreateIntersectionCurve(first, intersectionPoints1);
+				m_MainDataLayer.m_SceneDataLayer.m_SceneCAD.CreateIntersectionCurve(second, intersectionPoints2);
 			}
 			else
 			{
