55a9121f8e49790d65f69437b1ce87adb78a15db
Paweł Dębski <pawel.debski.99@gmail.com>
2024-01-01 13:01:08 +0100

    Moved the logic of calculating joint frames to a separate class

---
+++ BuD/PUMA/Robot/Calculators/EffectorFrameCalculator.cpp
@@ -0,0 +1,27 @@
+#include "EffectorFrameCalculator.h"
+
+auto EffectorFrameCalculator::Calculate(const RobotConfiguration& configuration, const RobotParameters& parameters) -> Frame
+{
+	auto F0 = Frame();
+
+	auto F1 = F0
+		.Rotate(dxm::Matrix::CreateFromAxisAngle(F0.Up(), DirectX::XMConvertToRadians(configuration.m_A1)));
+
+	auto F2 = F1
+		.Translate(parameters.m_L1 * F1.Up())
+		.Rotate(dxm::Matrix::CreateFromAxisAngle(F1.Front(), DirectX::XMConvertToRadians(configuration.m_A2)));
+
+	auto F3 = F2
+		.Translate(configuration.m_Q2 * F2.Right())
+		.Rotate(dxm::Matrix::CreateFromAxisAngle(F2.Front(), DirectX::XMConvertToRadians(configuration.m_A3)));
+
+	auto F4 = F3
+		.Translate(parameters.m_L3 * F3.Up())
+		.Rotate(dxm::Matrix::CreateFromAxisAngle(F3.Up(), DirectX::XMConvertToRadians(configuration.m_A4)));
+
+	auto F5 = F4
+		.Translate(parameters.m_L4 * F4.Right())
+		.Rotate(dxm::Matrix::CreateFromAxisAngle(F4.Right(), DirectX::XMConvertToRadians(configuration.m_A5)));
+
+	return F5;
+}
--- BuD/PUMA/Scene/PumaScene.cpp
+++ BuD/PUMA/Scene/PumaScene.cpp
@@ -1,6 +1,6 @@
 #include "PumaScene.h"
 
-#include <Robot/Frame.h>
+#include <Robot/Calculators/AllJointFramesCalculator.h>
 
 PumaScene::PumaScene()
 	: m_Scene()
@@ -11,26 +11,9 @@ PumaScene::PumaScene()
 
 auto PumaScene::UpdateRobotMesh(const RobotParameters& parameters, const RobotConfiguration& robotConfiguration) -> void
 {
-	auto F0 = Frame();
+	auto allJointsCalculator = AllJointFramesCalculator();
 
-	auto F1 = F0
-		.Rotate(dxm::Matrix::CreateFromAxisAngle(F0.Up(), DirectX::XMConvertToRadians(robotConfiguration.m_A1)));
-	
-	auto F2 = F1
-		.Translate(parameters.m_L1 * F1.Up())
-		.Rotate(dxm::Matrix::CreateFromAxisAngle(F1.Front(), DirectX::XMConvertToRadians(robotConfiguration.m_A2)));
-	
-	auto F3 = F2
-		.Translate(robotConfiguration.m_Q2 * F2.Right())
-		.Rotate(dxm::Matrix::CreateFromAxisAngle(F2.Front(), DirectX::XMConvertToRadians(robotConfiguration.m_A3)));
-
-	auto F4 = F3
-		.Translate(parameters.m_L3 * F3.Up())
-		.Rotate(dxm::Matrix::CreateFromAxisAngle(F3.Up(), DirectX::XMConvertToRadians(robotConfiguration.m_A4)));
-
-	auto F5 = F4
-		.Translate(parameters.m_L4 * F4.Right())
-		.Rotate(dxm::Matrix::CreateFromAxisAngle(F4.Right(), DirectX::XMConvertToRadians(robotConfiguration.m_A5)));
+	auto [F0, F1, F2, F3, F4, F5] = allJointsCalculator.Calculate(robotConfiguration, parameters);
 
 	m_RobotMesh->UpdateMesh({ F1.Position(), F2.Position(), F3.Position(), F4.Position(), F5.Position() });
 }
